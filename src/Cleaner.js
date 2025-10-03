0b50b446eda1a059684e49190a80acd066b3a35a
Youssef Louka <yousseflouka@Youssefs-MacBook-Pro.local>
2025-10-02 21:53:36 -0600

    added major performance imporvments

--- src/Cleaner.js
+++ src/Cleaner.js
@@ -2,134 +2,172 @@
 
 class Cleaner {
   constructor(options) {
-    this.options = options
+    this.options = options;
+    this._hop = Object.prototype.hasOwnProperty;
   }
 
   clean(data) {
-    if(!data) return data;
-    
+    if(data == null) return data;
+    if(typeof data !== 'object') return data;
+
     if(Array.isArray(data)) return this._cleanArray(data);
 
-    if(this._isObject(data)) return this.options.cleanInPlace? this._cleanObjectInPlace(data) : this._cleanObject(data);
+    if(!this._isPlainObject(data)) return data;
 
-    return data;
+    return this.options.cleanInPlace
+      ? this._cleanObjectInPlace(data)
+      : this._cleanObject(data);
   }
 
   _cleanArray(arr) {
-    return this.options.cleanInPlace ? this._getFilteredArrayInPlace(arr) : this._getFilteredArray(arr)
+    return this.options.cleanInPlace
+      ? this._getFilteredArrayInPlace(arr)
+      : this._getFilteredArray(arr);
   }
 
+  // Note: `for...in` iterates both own and inherited enumerable properties.
+  // For plain objects (created via `{}` or `new Object`), there are no enumerable prototype props,
+  // so this check will almost always be true.
+  // However, we keep the `hasOwnProperty` guard defensively to skip keys that may come
+  // from unexpected prototypes (e.g. `Object.create(null)` or custom prototype chains).
   _cleanObjectInPlace(object) {
-    Object.keys(object).forEach((key) => {
+    for (const key in object) {
+      if(!this._hop.call(object, key)) continue;
+
       const value = object[key];
 
-      if (Array.isArray(value)) {
+      if(Array.isArray(value)) {
         const mappedArr = this._cleanArray(value);
         if(this._shouldReturnArray(mappedArr, key)) object[key] = mappedArr;
         else delete object[key];
-      } else if (this._isObject(value)) {
+
+      } else if(this._isPlainObject(value)) {
         const subFiltered = this.clean(value);
         if(this._shouldReturnObject(subFiltered, key)) object[key] = subFiltered;
         else delete object[key];
-      } else {
-        if (this._shouldRemoveValue(value, key)) delete object[key]
-      }
-    })
 
-    return object
+      } else {
+        if(this._shouldRemoveValue(value, key)) delete object[key];
+      }
+    }
+
+    return object;
   }
 
   _cleanObject(object) {
     const filtered = {};
-  
-    Object.keys(object).forEach(key => {
+
+    for (const key in object) {
+      if(!this._hop.call(object, key)) continue;
+
       const value = object[key];
 
-      if (Array.isArray(value)) {
+      if(Array.isArray(value)) {
         const mappedArr = this._cleanArray(value);
-        if (this._shouldReturnArray(mappedArr, key)) filtered[key] = mappedArr;
-      } else if (this._isObject(value)) {
+        if(this._shouldReturnArray(mappedArr, key)) filtered[key] = mappedArr;
+
+      } else if(this._isPlainObject(value)) {
         const subFiltered = this.clean(value);
-        if (this._shouldReturnObject(subFiltered, key)) filtered[key] = subFiltered;
+        if(this._shouldReturnObject(subFiltered, key)) filtered[key] = subFiltered;
+
       } else {
-        if (!this._shouldRemoveValue(value, key)) filtered[key] = value;
+        if(!this._shouldRemoveValue(value, key)) filtered[key] = value;
       }
-    });
-  
+    }
+
     return filtered;
   }
 
-  _getFilteredArrayInPlace (arr) {
+  _getFilteredArrayInPlace(arr) {
     for (let i = arr.length - 1; i >= 0; i--) {
-      const item = arr[i];
-      const filteredItem = this.clean(item);
+      const filteredItem = this.clean(arr[i]);
 
-      if(this._isObject(filteredItem)) {
-        if(this._shouldReturnObject(filteredItem)) arr[i] = filteredItem
-        else arr.splice(i , 1);
-      }
-      else if(Array.isArray(filteredItem)) {
-        if (this._shouldReturnArray(filteredItem)) arr[i] = filteredItem
+      if(Array.isArray(filteredItem)) {
+        if(this._shouldReturnArray(filteredItem)) arr[i] = filteredItem;
         else arr.splice(i, 1);
-      }
-      else if(this._shouldRemoveValue(filteredItem)) arr.splice(i, 1);
-    }
 
+      } else if(this._isPlainObject(filteredItem)) {
+        if(this._shouldReturnObject(filteredItem)) arr[i] = filteredItem;
+        else arr.splice(i, 1);
+
+      } else {
+        if(this._shouldRemoveValue(filteredItem)) arr.splice(i, 1);
+      }
+    }
     return arr;
   }
 
-  _getFilteredArray (arr) {
-    const filteredArr = [];
-    arr.forEach(item => {
-      const filteredItem = this.clean(item);
-      if(this._isObject(filteredItem)) {
-        if(this._shouldReturnObject(filteredItem)) filteredArr.push(filteredItem);
+  _getFilteredArray(arr) {
+    const out = [];
+    for (let i = 0; i < arr.length; i++) {
+      const filteredItem = this.clean(arr[i]);
+
+      if(Array.isArray(filteredItem)) {
+        if(this._shouldReturnArray(filteredItem)) out.push(filteredItem);
+
+      } else if(this._isPlainObject(filteredItem)) {
+        if(this._shouldReturnObject(filteredItem)) out.push(filteredItem);
+
+      } else if(!this._shouldRemoveValue(filteredItem)) {
+        out.push(filteredItem);
       }
-      else if(Array.isArray(filteredItem)) {
-        if (this._shouldReturnArray(filteredItem)) filteredArr.push(filteredItem);
-      }
-      else if(!this._shouldRemoveValue(filteredItem)) filteredArr.push(filteredItem);
-    });
-  
-    return filteredArr;
+    }
+    return out;
   }
 
   _isUserOverride(key) {
-    if(!key || (!this.options.cleanKeys.has(key) && !this.options.skipKeys.has(key))) return undefined;
-    // higher priority to cleaning
-    if(this.options.cleanKeys.has(key)) return false;
-    if(this.options.skipKeys.has(key)) return true;
+    // Early exit when no key or not present in either Set
+    if(!key) return undefined;
+    const { cleanKeys, skipKeys } = this.options;
+    const inClean = cleanKeys.has(key);
+    const inSkip = skipKeys.has(key);
+    if(!inClean && !inSkip) return undefined;
+    // Higher priority to cleaning: if listed to clean => return false (i.e., do NOT skip)
+    if(inClean) return false;
+    if(inSkip) return true;
   }
 
-  _isObject (value) {
-    return Object.prototype.toString.call(value) === "[object Object]"
+  // Backward-compatible object test:
+  // - Includes: {} and class instances (new MyClass())
+  // - Excludes: null, arrays, Date/Map/Set/RegExp, etc.
+  _isPlainObject(value) {
+    // cheap guards first to avoid the toString call on obvious non-objects
+    if(value == null || typeof value !== 'object' || Array.isArray(value)) return false;
+    return Object.prototype.toString.call(value) === '[object Object]';
   }
 
-  _shouldReturnArray (arr, key) {
+  _isObjectNotEmpty(obj) {
+    // Cheap emptiness check without Object.keys allocation
+    for (const k in obj) {
+      if(this._hop.call(obj, k)) return true;
+    }
+    return false;
+  }
+
+  _shouldReturnArray(arr, key) {
     const userOverride = this._isUserOverride(key);
     if(userOverride !== undefined) return userOverride;
-    return !this.options.emptyArraysCleaner || arr.length !== 0
+    // If not cleaning empty arrays, always keep; else require non-empty
+    return !this.options.emptyArraysCleaner || arr.length !== 0;
   }
 
-  _shouldReturnObject (object, key) {
+  _shouldReturnObject(object, key) {
     const userOverride = this._isUserOverride(key);
     if(userOverride !== undefined) return userOverride;
-    return !this.options.emptyObjectsCleaner || Object.keys(object).length !== 0
+    // Avoid Object.keys(object).length allocation
+    return !this.options.emptyObjectsCleaner || this._isObjectNotEmpty(object);
   }
 
-  _shouldRemoveValue (value, key) {
+  _shouldRemoveValue(value, key) {
     const userOverride = this._isUserOverride(key);
     if(userOverride !== undefined) return !userOverride;
 
-    if(value === null) return this.options.nullCleaner
-  
-    if(value === "") return this.options.emptyStringsCleaner
-  
-    if(Number.isNaN(value)) return this.options.nanCleaner
+    if(value === undefined) return true;
+    if(value === null) return !!this.options.nullCleaner;
+    if(value === '') return !!this.options.emptyStringsCleaner;
+    if(Number.isNaN(value)) return !!this.options.nanCleaner;
 
-    if(value === undefined) return true
-  
-    return false
+    return false;
   }
 }
 
